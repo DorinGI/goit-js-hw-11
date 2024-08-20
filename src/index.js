17f703f9af17aa43cb1a4b89b7095ba0bc440596
DorinGI <158717197+DorinGI@users.noreply.github.com>
2024-08-20 19:52:47 +0200

    functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import { fetchImages } from './api';
 
 const form = document.querySelector('#search-form');
+const loadMoreButton = document.querySelector('.load-more');
+
+let page = 1;
+let query = '';
 
 const onFormSubmit = e => {
   e.preventDefault();
@@ -12,6 +16,13 @@ const onFormSubmit = e => {
 const onSearch = async query => {
   const images = await fetchImages(query);
   console.log(images);
+  renderGallery(images.hits);
+  loadMoreButton.style.display = 'block';
+};
+
+const onLoadMore = async query => {
+  page++;
+  const images = await fetchImages(query, page);
   renderGallery(images.hits);
 };
 
@@ -56,3 +67,4 @@ const renderGallery = images => {
 };
 
 form.addEventListener('submit', onFormSubmit);
+loadMoreButton.addEventListener('click', onLoadMore);
