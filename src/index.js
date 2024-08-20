cd98b3a24c4bc8a3dbb182ed3a0fbdbd003196f8
DorinGI <158717197+DorinGI@users.noreply.github.com>
2024-08-20 20:53:47 +0200

    final commit

--- src/index.js
+++ src/index.js
@@ -1,33 +1,59 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { fetchImages } from './api';
+import { Notify } from 'notiflix';
 
 const form = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
 const loadMoreButton = document.querySelector('.load-more');
 
 let page = 1;
 let query = '';
+const parameters = {
+  position: 'center-center',
+  timeout: 4000,
+  width: '550px',
+  fontSize: '30px',
+};
 
 const onFormSubmit = e => {
   e.preventDefault();
   const query = e.currentTarget.elements.searchQuery.value;
   console.log(query);
-  onSearch(query);
+  if (query === '') {
+    Notify.info('Please enter something to search!', parameters);
+    return;
+  } else {
+    onSearch(query);
+  }
 };
 
 const onSearch = async query => {
   const images = await fetchImages(query);
   console.log(images);
-  renderGallery(images.hits);
-  loadMoreButton.style.display = 'block';
+
+  if (images.totalHits === 0) {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.',
+      parameters
+    );
+  } else {
+    Notify.info(`We found ${images.totalHits} images.`, parameters);
+    gallery.innerHTML = '';
+    renderGallery(images.hits);
+    loadMoreButton.style.display = 'block';
+    lightbox.refresh();
+  }
 };
 
 const onLoadMore = async query => {
   page++;
   const images = await fetchImages(query, page);
   renderGallery(images.hits);
+  lightbox.refresh();
 };
 
 const renderGallery = images => {
-  const gallery = document.querySelector('.gallery');
   gallery.insertAdjacentHTML(
     'beforeend',
     images
@@ -35,7 +61,7 @@ const renderGallery = images => {
         ({
           id,
           webformatURL,
-          largeImageUrl,
+          largeImageURL,
           tags,
           likes,
           views,
@@ -43,9 +69,11 @@ const renderGallery = images => {
           downloads,
         }) =>
           `<div key='${id}' class="photo-card">
-            <a href='${largeImageUrl}' target='_blank'>    
-                <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-            </a>    
+            <div class="image_wrap">
+                <a href='${largeImageURL}'>    
+                    <img src="${webformatURL}" alt="${tags}" width="250" loading="lazy" />
+                </a>    
+            </div>
             <div class="info">
                 <p class="info-item">
                 <b>Likes ${likes}</b>
@@ -68,3 +96,8 @@ const renderGallery = images => {
 
 form.addEventListener('submit', onFormSubmit);
 loadMoreButton.addEventListener('click', onLoadMore);
+
+let lightbox = new SimpleLightbox('.image_wrap a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
